c042fa62ba83f9ac51a5a7117185f70430b0363b
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-11 22:51:07 +0530

    Update code.py

--- code.py
+++ code.py
@@ -39,6 +39,7 @@ df_subjects = pd.merge(df_subjects, subject_topics, left_on='id',right_on='subje
 df_subjects = df_subjects.loc[:,~df_subjects.columns.duplicated()]
 df_subjects = pd.merge(df_subjects,df_tags,left_on='topic_id',right_on='id',suffixes=('', '_x'))
 df_subjects = df_subjects.loc[:,~df_subjects.columns.duplicated()]
+goals = ['Start my Career with a Spring Week','Get a Summer Internship','Get an Internship alongside my Studies', 'Land a Placement Year','Win Awards & Competitions','Secure a Graduate Job','Find a Co-founder & Start a Business', 'Meet Like-minded Students & join Societies','Expand my Network & Connect with Industry Leaders']
 Goals =  st.multiselect('Enter the goals',goals,key = "one")
 interest = st.multiselect('Enter the interest',df_T['name'].unique(),key = "two")
 weight = [1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,1,2,1]
