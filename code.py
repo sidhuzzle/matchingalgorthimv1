5a12b898d4ec88ca3211f835437674e8c6260dd3
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-11 12:05:04 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import psycopg2 as pg
 import numpy as np
+@st.cache(ttl=24*3600)
 engine = pg.connect("dbname='huzzle_production' user='postgres' host='huzzle-production-db-read.ct4mk1ahmp9p.eu-central-1.rds.amazonaws.com' port='5432' password='S11mXHLGbA0Cb8z8uLfj'")
 df_touchpoints = pd.read_sql('select * from touchpoints', con=engine)
 df_tags = pd.read_sql('select * from tags', con=engine)
@@ -12,6 +13,8 @@ df_cities.rename(columns = {'name':'city_name'}, inplace = True)
 df_subjects = pd.read_sql('select * from subjects', con=engine)
 subject_topics = pd.read_sql('select * from subjects_topics', con=engine)
 df_degrees = pd.read_sql('select * from degrees', con=engine)
+df_degrees.replace("Bachelor's","Bachelors", inplace=True)
+df_degrees.replace("Master's","Masters", inplace=True)
 df_tc = pd.read_sql('select * from touchpoints_cities', con=engine)
 df_goals = pd.read_sql('select * from goals', con=engine)
 df_goal_weights = pd.read_sql('select * from matching_goal_weights', con=engine)
@@ -35,3 +38,19 @@ df_subjects = pd.merge(df_subjects, subject_topics, left_on='id',right_on='subje
 df_subjects = df_subjects.loc[:,~df_subjects.columns.duplicated()]
 df_subjects = pd.merge(df_subjects,df_tags,left_on='topic_id',right_on='id',suffixes=('', '_x'))
 df_subjects = df_subjects.loc[:,~df_subjects.columns.duplicated()]
+goals = ['Start my Career with a Spring Week','Get a Summer Internship','Get an Internship alongside my Studies', 'Land a Placement Year','Win Awards & Competitions','Secure a Graduate Job','Find a Co-founder & Start a Business', 'Meet Like-minded Students & join Societies','Expand my Network & Connect with Industry Leaders']
+Goals =  st.multiselect('Enter the goals',goals,key = "one")
+group_6 = df.groupby(df.type)
+df_T = group_6.get_group("Topic")
+interest = st.multiselect('Enter the interest',df_T['name'].unique(),key = "two")
+weight = [1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,1,2,1]
+Weight = st.multiselect('Enter the weight',weight,key = "three")
+Interest = pd.DataFrame(interest,columns = ['Interest'])
+Weight = pd.DataFrame(Weight,columns = ['Weight'])
+df_interest = pd.concat([Interest,Weight],axis = 1)
+University = st.selectbox('Enter the university',df_universities['name'].unique(),key = 'four')
+Subject = st.selectbox('Enter the subject',df_subjects['name'].unique(),key = 'five')
+Degree =  st.selectbox('Enter the degree',df_degrees['name'].unique(),key = 'six')
+year = ['First Year ','Second Year','Third Year','Final Year']
+Year = st.selectbox('Enter the year',year,key = 'seven')
+st.write(df)
