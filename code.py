b1a49a89f3cc9aa7e27574a4f24af69bb097a57f
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-11 14:57:29 +0530

    Update code.py

--- code.py
+++ code.py
@@ -59,4 +59,19 @@ if st.button('Submit',key = 'eight'):
     df_goals = df_goals.loc[:,~df_goals.columns.duplicated()]
     df =  pd.merge(df, df_goals, left_on='kind',right_on='touchpointable_kind',suffixes=('', '_x'),how = 'inner')
     df = df.loc[:,~df.columns.duplicated()]
-    st.write(df)
+    if len(interest) > 0:
+  group_7 = df.groupby(df.type)
+  df_I = group_7.get_group("Topic")
+  df_I =  pd.merge(df_I, df_interest, left_on='name',right_on='Interest',suffixes=('', '_x'),how = 'inner')
+  df_I = df_I.loc[:,~df_I.columns.duplicated()]
+  col_list = df_I['name'].unique()
+  df_I['idx'] = df_I.groupby(['touchpointable_id', 'name']).cumcount()
+  df_I = df_I.pivot(index=['idx','touchpointable_id'], columns='name', values='Weight').sort_index(level=1).reset_index().rename_axis(None, axis=1)
+  df_I = df_I.fillna(0)
+  df_I['Weight'] = df_I[col_list].sum(axis=1)
+  df_I = pd.merge(df, df_I, left_on='touchpointable_id',right_on='touchpointable_id',suffixes=('', '_x'),how = 'inner')
+  df_I = df_I.loc[:,~df_I.columns.duplicated()]
+    if len(interest) == 0:
+      df_I = df
+      df_I['Weight'] = 0
+      st.write(df_I)
