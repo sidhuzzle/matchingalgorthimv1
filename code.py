58d83786c8ed156aec2d2fec16fdbcfa02e0f161
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-11 11:56:16 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import psycopg2 as pg
+import numpy as np
+engine = pg.connect("dbname='huzzle_production' user='postgres' host='huzzle-production-db-read.ct4mk1ahmp9p.eu-central-1.rds.amazonaws.com' port='5432' password='S11mXHLGbA0Cb8z8uLfj'")
+df_touchpoints = pd.read_sql('select * from touchpoints', con=engine)
+df_tags = pd.read_sql('select * from tags', con=engine)
+df_tagging = pd.read_sql('select * from taggings', con=engine)
+df_universities = pd.read_sql('select * from universities', con=engine)
+f_cities = pd.read_sql('select * from cities', con=engine)
+df_cities.rename(columns = {'name':'city_name'}, inplace = True)
+df_subjects = pd.read_sql('select * from subjects', con=engine)
+subject_topics = pd.read_sql('select * from subjects_topics', con=engine)
+df_degrees = pd.read_sql('select * from degrees', con=engine)
+df_tc = pd.read_sql('select * from touchpoints_cities', con=engine)
+df_goals = pd.read_sql('select * from goals', con=engine)
+df_goal_weights = pd.read_sql('select * from matching_goal_weights', con=engine)
+grouped_1 = df_touchpoints.groupby(df_touchpoints.state)
+df_touchpoints = grouped_1.get_group(1)
+df_touchpoints =  pd.merge(df_touchpoints, df_tagging, left_on='id',right_on='taggable_id',suffixes=('', '_x'))
+df_touchpoints = df_touchpoints.loc[:,~df_touchpoints.columns.duplicated()]
+df_touchpoints = pd.merge(df_touchpoints,df_tags,left_on='tag_id',right_on='id',suffixes=('', '_x'))
+df = df_touchpoints.loc[:,~df_touchpoints.columns.duplicated()]
+df = pd.merge(df,df_tc,left_on='id',right_on='touchpoint_id',suffixes=('', '_x'),how = 'left')
+df = df.loc[:,~df.columns.duplicated()]
+df = pd.merge(df,df_cities,left_on='city_id',right_on='id',suffixes=('', '_x'),how = 'left')
+df = df.loc[:,~df.columns.duplicated()]
+df_universities_1 = pd.merge(df_universities, df_cities, left_on='city_id',right_on='id',suffixes=('', '_x'),how = 'left')
+df_universities_1 = df_universities_1.loc[:,~df_universities_1.columns.duplicated()]
+df_goals = pd.merge(df_goals, df_goal_weights, left_on='id',right_on='goal_id',suffixes=('', '_x'),how = 'inner')
+df_goals = df_goals.loc[:,~df_goals.columns.duplicated()]
+df_goals = df_goals[['id','title','touchpointable_kind','value']].copy()
+df_goals.rename(columns = {'title':'goal'}, inplace = True)
+df_subjects = pd.merge(df_subjects, subject_topics, left_on='id',right_on='subject_id',suffixes=('', '_x'),how = 'inner')
+df_subjects = df_subjects.loc[:,~df_subjects.columns.duplicated()]
+df_subjects = pd.merge(df_subjects,df_tags,left_on='topic_id',right_on='id',suffixes=('', '_x'))
+df_subjects = df_subjects.loc[:,~df_subjects.columns.duplicated()]
