15cc5dbd472f8148ae0b942159b6767e95058b65
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-12 18:54:23 +0530

    Update code.py

--- code.py
+++ code.py
@@ -21,7 +21,7 @@ Subject = st.selectbox('Enter the subject',df_subjects['name'].unique(),key = 'f
 Degree =  st.selectbox('Enter the degree',df_degrees['name'].unique(),key = 'six')
 Year = st.selectbox('Enter the year',year,key = 'seven')
 
-@st.cache(ttl=72*7200)
+@st.cache(ttl=100*10000)
 @st.cache(suppress_st_warning=True)
 def matching_algo():
   
