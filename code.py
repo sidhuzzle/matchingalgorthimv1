7aedccb19cf2777eaa33196a57049d10941e8740
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-12 16:11:51 +0530

    Update code.py

--- code.py
+++ code.py
@@ -139,7 +139,7 @@ def user_input():
     df = df.loc[:,~df.columns.duplicated()]
     df = df[['id','touchpointable_id','type','touchpointable_type','kind','title','name','creatable_for_name','Weight','city_name','city score','degree score','subject score','year score','value','matching score']].copy()
     df = df.sort_values(by='matching score',ascending=False)
-    
+    st.write(df)
 user_input()  
-st.write(df)
+
 
