f393e854eba767fb32583ab787ea1b442d8c6018
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-18 18:10:06 +0530

    Update code.py

--- code.py
+++ code.py
@@ -144,7 +144,7 @@ def matching_algo(Goals,Interest,weight,University,Degree,Subject,Year):
     df_1 = pd.DataFrame(df_1)
     n = df_1['value'].iloc[0]
     n = round(len(df_1)*(n/10))
-    df = df_1.head(n)
+  df = df_1.head(n)
 
 Goals =  st.multiselect('Enter the goals',df_goals['title'].unique(),key = "one")
 Interest = st.multiselect('Enter the interest',df_tags['name'].unique(),key = "two")
