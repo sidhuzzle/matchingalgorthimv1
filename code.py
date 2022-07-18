001b592506d1ae4a5d648d915ecd1fd27a72610d
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-18 18:07:18 +0530

    Update code.py

--- code.py
+++ code.py
@@ -144,6 +144,7 @@ def matching_algo(Goals,Interest,weight,University,Degree,Subject,Year):
     df_1 = pd.DataFrame(df_1)
     n = df_1['value'].iloc[0]
     n = round(len(df_1)*(n/10))
+    df = df_1.head(n)
 
 Goals =  st.multiselect('Enter the goals',df_goals['title'].unique(),key = "one")
 Interest = st.multiselect('Enter the interest',df_tags['name'].unique(),key = "two")
@@ -154,8 +155,6 @@ Subject = st.selectbox('Enter the subject',df_subjects['name'].unique(),key = 'f
 Degree =  st.selectbox('Enter the degree',df_degrees['name'].unique(),key = 'six')
 Year = st.selectbox('Enter the year',year,key = 'seven')
 
-  
-
-df = matching_algo(Goals,Interest,weight,University,Degree,Subject,Year)
+matching_algo(Goals,Interest,weight,University,Degree,Subject,Year)
 if st.button("Submit",key = "eight"):
   st.write(df)
