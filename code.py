bd24a75698d418d38fd37996a6bd95afea42fb16
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-19 20:29:20 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,16 +3,17 @@ import pandas as pd
 import psycopg2 as pg
 import numpy as np
 
-engine = pg.connect("dbname='huzzle_production' user='postgres' host='huzzle-production-db-read.ct4mk1ahmp9p.eu-central-1.rds.amazonaws.com' port='5432' password='S11mXHLGbA0Cb8z8uLfj'")
-df_goals = pd.read_sql('select * from goals', con=engine)
-df_tags = pd.read_sql('select * from tags', con=engine)
-df_universities = pd.read_sql('select * from universities', con=engine)
-df_degrees = pd.read_sql('select * from degrees', con=engine)
-df_subjects = pd.read_sql('select * from subjects', con=engine)
-weight = [1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,1]
-year = ['1','2','3','4']
-@st.cache(hash_funcs={psycopg2:id})
+#engine = pg.connect("dbname='huzzle_production' user='postgres' host='huzzle-production-db-read.ct4mk1ahmp9p.eu-central-1.rds.amazonaws.com' port='5432' password='S11mXHLGbA0Cb8z8uLfj'")
+#df_goals = pd.read_sql('select * from goals', con=engine)
+#df_tags = pd.read_sql('select * from tags', con=engine)
+#df_universities = pd.read_sql('select * from universities', con=engine)
+#df_degrees = pd.read_sql('select * from degrees', con=engine)
+#df_subjects = pd.read_sql('select * from subjects', con=engine)
+#weight = [1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,1]
+#year = ['1','2','3','4']
+@st.cache()
 def matching_algo(Goals,Interest,weight,University,Degree,Subject,Year):
+  engine = pg.connect("dbname='huzzle_production' user='postgres' host='huzzle-production-db-read.ct4mk1ahmp9p.eu-central-1.rds.amazonaws.com' port='5432' password='S11mXHLGbA0Cb8z8uLfj'")
   df_touchpoints = pd.read_sql('select * from touchpoints', con=engine)
   grouped_1 = df_touchpoints.groupby(df_touchpoints.state)
   df_touchpoints = grouped_1.get_group(1)
