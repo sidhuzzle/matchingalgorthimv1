d8ee143f834f35519512a9f8eb3f21c4d34f9490
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-28 23:22:49 +0530

    Update code.py

--- code.py
+++ code.py
@@ -157,9 +157,7 @@ def matching_algo(Goals,Interest,weight,University,Degree,Subject,Year):
     df_T = group_3.get_group('Topic')
     df_S = pd.merge(df_T,df_subjects_1, left_on='name',right_on='name_x',suffixes=('', '_x'),how = 'inner')
     df_S = df_S.loc[:,~df_S.columns.duplicated()]
-    df_touchpoints = df_S
     df_S =  df_S.groupby('id', as_index=False).first()
-    
     df_S = pd.merge(df_touchpoints, df_S, left_on='touchpointable_id',right_on='touchpointable_id',suffixes=('', '_x'),how = 'inner')
     df_S = df_S.loc[:,~df_S.columns.duplicated()]
     id = df_S['id'].to_list()
