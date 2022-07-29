d5be88136f20c9d5e39362abb1521b8bb1609336
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-29 17:02:50 +0530

    Update code.py

--- code.py
+++ code.py
@@ -182,7 +182,7 @@ def matching_algo(Goals,Interest,weight,University,Degree,Subject,Year):
   
   df = df_touchpoints[['id','touchpointable_id','type','touchpointable_type','kind','title','name','creatable_for_name','Weight','city_name','city score','degree score','subject score','year score','value']].copy()
   col_list = ['Weight','city score','degree score','subject score','year score']
-  df['matching score'] = df[col_list].sum(axis=1)
+  #df['matching score'] = df[col_list].sum(axis=1)
   #df = df.groupby(['id','touchpointable_id','type','touchpointable_type','kind','title','name','creatable_for_name','Weight','city_name','city score','degree score','subject score','year score','value']).sum()
   #df = df.groupby('id', as_index=False).first()
   df = df.set_index(['id', df.groupby('id').cumcount()])['name'].unstack().add_prefix('name').reset_index()
