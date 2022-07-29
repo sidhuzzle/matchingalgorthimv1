9db67f29728fe896db82d16da7a586ae8a20a453
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-30 03:57:07 +0530

    Update code.py

--- code.py
+++ code.py
@@ -189,19 +189,19 @@ def matching_algo(Goals,Interest,weight,University,Degree,Subject,Year):
   #df = df_T.set_index(['id', df.groupby('id').cumcount()])['name'].unstack().add_prefix('name').reset_index()
   
   
-  #df_matches = df.groupby('id', as_index=False).first()
+  df_matches = df.groupby('id', as_index=False).first()
   #df = df.groupby(['id','touchpointable_id','type','touchpointable_type','kind','title','name','creatable_for_name','Weight','city_name','city score','degree score','subject score','year score','value']).sum()
-  #df_matches = df[['id','matching score']].copy()
+  df_matches = df[['id','matching score']].copy()
   
   
-  #df_touchpoints['idx'] = df.groupby(['id','type']).cumcount()
-  #df_name =  df_touchpoints.pivot(index=['idx','id'], columns='type', values='name').sort_index(level=1).reset_index().rename_axis(None, axis=1)
-  #df_name = df_name.set_index(['id', df_name.groupby('id').cumcount()])['Topic'].unstack().add_prefix('Topic').reset_index()
-  #df_name = pd.DataFrame(df_name.to_records())
-  #df = pd.merge(df, df_name, left_on='id',right_on='id',suffixes=('', '_x'),how = 'inner')
-  #df = df.loc[:,~df.columns.duplicated()]
-  #a = len(df) * .60
-  #df = df.dropna(axis=0, thresh=a, how="any")
+  df_touchpoints['idx'] = df.groupby(['id','type']).cumcount()
+  df_name =  df_touchpoints.pivot(index=['idx','id'], columns='type', values='name').sort_index(level=1).reset_index().rename_axis(None, axis=1)
+  df_name = df_name.set_index(['id', df_name.groupby('id').cumcount()])['Topic'].unstack().add_prefix('Topic').reset_index()
+  df_name = pd.DataFrame(df_name.to_records())
+  df = pd.merge(df, df_name, left_on='id',right_on='id',suffixes=('', '_x'),how = 'inner')
+  df = df.loc[:,~df.columns.duplicated()]
+  a = len(df) * .60
+  df = df.dropna(axis=0, thresh=a)
   
   #df = pd.merge(df, df_matches, left_on='id',right_on='id',suffixes=('', '_x'),how = 'inner')
   #df = df.loc[:,~df.columns.duplicated()]
