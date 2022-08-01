e89cdd24e4731a28ba64daec13e7da15acbb429e
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-08-01 17:12:41 +0530

    Update code.py

--- code.py
+++ code.py
@@ -233,15 +233,15 @@ if st.button("Submit",key = "eight"):
   
   
   df = matching_algo(Goals,Interest,weight,University,Degree,Subject,Year)
-  if len(df['value'].unique()) > 1:
-   kind = df.groupby("kind")
-   for group,df_1 in kind:
-    df_1 = pd.DataFrame(df_1)
-    n = df_1['value'].iloc[0]
-    n = round(len(df_1)*(n/10))
-    df = df_1.head(n)
-    l = round(len(df)*(12/100))
-    df = df.head(l)
+  #if len(df['value'].unique()) > 1:
+  # kind = df.groupby("kind")
+   #for group,df_1 in kind:
+    #df_1 = pd.DataFrame(df_1)
+    #n = df_1['value'].iloc[0]
+    #n = round(len(df_1)*(n/10))
+    #df = df_1.head(n)
+    #l = round(len(df)*(12/100))
+    #df = df.head(l)
     id = df['touchpointable_id'].to_list()
     st.write(df)
     
