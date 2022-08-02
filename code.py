b861206db5ebd5c25fcf6e2a8d04bfb790ba940c
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-08-03 00:13:59 +0530

    Update code.py

--- code.py
+++ code.py
@@ -243,7 +243,7 @@ if st.button("Submit",key = "eight"):
       df_1 = pd.DataFrame(df_1)
       n = df_1['value'].iloc[0]
       n = len(df_1)*(n/10)
-      if n >=0.5:
+      if n >= 0.5:
         n = 1
         
         df = df_1.head(n)
