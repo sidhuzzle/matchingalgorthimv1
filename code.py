4f6fe89364363a8c0945af939c8d58e648827b16
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-08-02 12:58:14 +0530

    Update code.py

--- code.py
+++ code.py
@@ -245,12 +245,12 @@ if st.button("Submit",key = "eight"):
       n = round(len(df_1)*(n/10))
       #df = df_1.head(n)
       l.append(n)
-    l = list(filter(lambda x: x != 0, l))
-    total = sum(l)
-    for x in range(0,len(l)):
+      l = list(filter(lambda x: x != 0, l))
+      total = sum(l)
+    #for x in range(0,len(l)):
       n = round(l[x]*100/total * 12/100)
       df = df_1.head(n)
-    st.write(df)
+      st.write(total)
     
   else:
     group_0 = df.groupby(df.touchpointable_type)
