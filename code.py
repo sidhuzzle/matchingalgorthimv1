56c31f2e5f9454f7355acc03e4404b2132c4889f
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-08-02 12:42:24 +0530

    Update code.py

--- code.py
+++ code.py
@@ -244,11 +244,11 @@ if st.button("Submit",key = "eight"):
       n = df_1['value'].iloc[0]
       n = round(len(df_1)*(n/10))
       #df = df_1.head(n)
-      l.append(n)
-    l = list(filter(lambda x: x != 0, l))
-    for x in range(0,len(l)):
-      total = total + l[x]
-      n = round(l[x]*100/total * 12/100) 
+      #l.append(n)
+    #l = list(filter(lambda x: x != 0, l))
+    #for x in range(0,len(l)):
+      #total = total + l[x]
+      #n = round(l[x]*100/total * 12/100) 
       st.write(n)
     
   else:
