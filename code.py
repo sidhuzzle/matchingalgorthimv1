17607b275f9a213c5029e887dd61fa6ec41c653f
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-08-03 15:23:59 +0530

    Update code.py

--- code.py
+++ code.py
@@ -236,21 +236,22 @@ if st.button("Submit",key = "eight"):
       n = round(len(df_1)*(n/10))
       l.append(n)
       df_K.append(df_1)
+      st.write(n)
     
-    total = sum(l)
+    #total = sum(l)
     
-    total = sum(l)
-    df_2 = pd.concat(df_K)
-    kind_2 = df_2.groupby(["kind","value"])
+    #total = sum(l)
+    #df_2 = pd.concat(df_K)
+    #kind_2 = df_2.groupby(["kind","value"])
     
-    for x,y in kind_2:
-      y = pd.DataFrame(y)
-      n = y['value'].iloc[0]
-      n = round(len(y)*(n/10))
-      n = round(n*100/total * 12/100)
-      df = df_2.head(n)
-      df = df.sort_values(by='matching score',ascending=False)
-      st.write(df)
+    #for x,y in kind_2:
+      #y = pd.DataFrame(y)
+      #n = y['value'].iloc[0]
+      #n = round(len(y)*(n/10))
+      #n = round(n*100/total * 12/100)
+      #df = df_2.head(n)
+      #df = df.sort_values(by='matching score',ascending=False)
+      #st.write(df)
      
   else:
     group_0 = df.groupby(df.touchpointable_type)
